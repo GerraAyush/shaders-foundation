c714de5791894591d4986d84f04b6a3856a189a1
GerraAyush <ayushgerra3@gmail.com>
2026-02-02 19:33:08 -0500

    added third dimension and more shapes

---
+++ Assets/Scripts/FunctionLibrary.cs
@@ -0,0 +1,106 @@
+using UnityEngine;
+
+using static UnityEngine.Mathf;
+
+public class FunctionLibrary {
+
+    public delegate Vector3 Function (float u, float v, float t);
+    public enum FunctionName { Wave, MultiWave, Ripple, Sphere, SphereVerticalBands, SphereHorizontalBands, SphereTwstingBands, Torus, TwistingTorus }
+    private static readonly Function[] functions = { Wave, MultiWave, Ripple, Sphere, SphereVerticalBands, SphereHorizontalBands, SphereTwstingBands, Torus, TwistingTorus };
+
+    // The divisions are placed at the end of the function to ensure that the values of the wave are in the range -1 to 1
+    public static Vector3 Wave(float u, float v, float t) {
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (u + v + t));
+        p.z = v;
+        return p;
+    }
+
+    public static Vector3 MultiWave(float u, float v, float t) {
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (u + 0.5f * t));
+        p.y += Sin(2f * PI * (v + t)) * 0.5f;
+        p.y += Sin(PI * (u + v + 0.25f * t));
+        p.y *= (1f / 2.5f);
+        p.z = v;
+        return p;
+    }
+
+    public static Vector3 Ripple (float u, float v, float t) {
+		float d = Sqrt(u * u + v * v);
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(4f * PI * d - t);
+		p.y /= (1 + 10f * d);
+        p.z = v;
+        return p;
+	}
+
+    public static Vector3 Sphere (float u, float v, float t) {
+		float r = 0.5f + 0.5f * Sin(PI * t);
+		float s = r * Cos(0.5f * PI * v);
+		Vector3 p;
+		p.x = s * Sin(PI * u);
+		p.y = r * Sin(0.5f * PI * v);
+		p.z = s * Cos(PI * u);
+        return p;
+	}
+
+    public static Vector3 SphereVerticalBands (float u, float v, float t) {
+		float r = 0.9f + 0.1f * Sin(8f * PI * u);
+		float s = r * Cos(0.5f * PI * v);
+		Vector3 p;
+		p.x = s * Sin(PI * u);
+		p.y = r * Sin(0.5f * PI * v);
+		p.z = s * Cos(PI * u);
+        return p;
+	}
+
+    public static Vector3 SphereHorizontalBands (float u, float v, float t) {
+		float r = 0.9f + 0.1f * Sin(8f * PI * v);
+		float s = r * Cos(0.5f * PI * v);
+		Vector3 p;
+		p.x = s * Sin(PI * u);
+		p.y = r * Sin(0.5f * PI * v);
+		p.z = s * Cos(PI * u);
+        return p;
+	}
+
+    public static Vector3 SphereTwstingBands (float u, float v, float t) {
+		float r = 0.9f + 0.1f * Sin(PI * (6f * u + 4f * v + t));
+		float s = r * Cos(0.5f * PI * v);
+		Vector3 p;
+		p.x = s * Sin(PI * u);
+		p.y = r * Sin(0.5f * PI * v);
+		p.z = s * Cos(PI * u);
+        return p;
+	}
+
+    public static Vector3 Torus (float u, float v, float t) {
+		float r1 = 0.75f;
+		float r2 = 0.25f;
+		float s = r1 + r2 * Cos(PI * v);
+		Vector3 p;
+		p.x = s * Sin(PI * u);
+		p.y = r2 * Sin(PI * v);
+		p.z = s * Cos(PI * u);
+        return p;
+	}
+
+    public static Vector3 TwistingTorus (float u, float v, float t) {
+		float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
+		float r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
+		float s = r1 + r2 * Cos(PI * v);
+		Vector3 p;
+		p.x = s * Sin(PI * u);
+		p.y = r2 * Sin(PI * v);
+		p.z = s * Cos(PI * u);
+        return p;
+	}
+
+    public static Function GetFunction (FunctionName name) {
+        return functions[(int) name];
+    }
+}
--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -5,21 +5,17 @@ public class Graph : MonoBehaviour
 
 	[SerializeField] private Transform pointPrefab;
 	[SerializeField, Range(10, 100)] private int resolution = 10;
+	[SerializeField] private FunctionLibrary.FunctionName function = 0;
 
         private Transform[] points;
 	
 	void Awake () {
                 float step = 2f / resolution;
-                Vector3 position = Vector3.zero;
                 var scale = Vector3.one * step;
 
-                points = new Transform[resolution];
+                points = new Transform[resolution * resolution];
                 for (int idx = 0; idx < points.Length; idx++) {
-                        Transform point = Instantiate(pointPrefab);
-                        points[idx] = point;
-
-                        position.x = (idx + 0.5f) * step - 1f;
-                        point.localPosition = position;
+                        Transform point = points[idx] = Instantiate(pointPrefab);
                         point.localScale = scale; // Each cube has size 1 in each dimension by default, so to make them fit we have to reduce their scale
                         point.SetParent(transform, false);
                 }
@@ -27,11 +23,18 @@ public class Graph : MonoBehaviour
 
         void Update() {
                 float time = Time.time;
-                for (int idx = 0; idx < points.Length; idx++) {
-                        Transform point = points[idx];
-                        Vector3 position = point.localPosition;
-                        position.y = Mathf.Sin(Mathf.PI * (position.x + time));
-                        point.localPosition = position;
+                float step = 2f / resolution;
+                FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
+
+                float v = 0.5f * step - 1f;
+                for (int idx = 0, x = 0, z = 0; idx < points.Length; idx++, x++) {
+                        if (x == resolution) {
+                                x = 0;
+                                z += 1;
+                                v = (z + 0.5f) * step - 1f;
+                        }
+                        float u = (x + 0.5f) * step - 1;
+                        points[idx].localPosition = f(u, v, time);
                 }
         }
 }
